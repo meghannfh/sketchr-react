3ebb7dc63447cc1b6d86b67f607a4099cc786df0
Meghann Hlibichuk <meghannfh@gmail.com>
2024-02-13 02:20:23 -0600

    checked for matching passwords logic

--- client/src/components/forms/SignupForm.jsx
+++ client/src/components/forms/SignupForm.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import { BsEyeSlash, BsEye } from 'react-icons/bs'
 
-const SignupForm = ({ handleSignup, error, isLoading, formRef }) => {
-  const [showEye, setShowEye] = useState(false)
-  const [isPassingPwd, setIsPassingPwd] = useState(false);
-  const [pwdNotMatchingCriteria, setPwdNotMatchingCriteria] = useState("none");
-  console.log(pwdNotMatchingCriteria)
+const SignupForm = ({ 
+  handleSignup, 
+  error, 
+  isLoading, 
+  formRef, 
+  setIsPassingPwd, 
+  isPassingPwd,
+  setPwdNotMatchingCriteria, 
+  pwdNotMatchingCriteria }) => {
+
+  const [showEye, setShowEye] = useState(false);
+  const [firstPwd, setFirstPwd] = useState("");
 
   const handleToggleEye = () => {
     setShowEye(prevShowEye => !prevShowEye)
+  }
+
+  const checkMatchingPwd = (validatedPwd) => {
+    if(firstPwd === validatedPwd) {
+      setIsPassingPwd(true)
+    }
   }
 
   const checkPwdRequirements = (pwd) => {
@@ -44,21 +57,16 @@ const SignupForm = ({ handleSignup, error, isLoading, formRef }) => {
       notPassingCriteria.push("special")
     }
 
+    setFirstPwd(pwd)
     setPwdNotMatchingCriteria(notPassingCriteria)
   }
+
+
     return (
     <div className="grid h-screen place-content-center">
       <div className="forms-styles">
         <form className="flex flex-col gap-2" onSubmit={handleSignup} ref={formRef} encType="multipart/form-data">
           <h3 className="font-bold text-xl">Sign Up</h3>
-          {/* <div className="form-layout">
-            <input 
-              type="text" 
-              name="username"
-              className='border-2'
-              placeholder="username" 
-            />
-          </div> */}
           <div className="form-layout">
             <input 
               type="text" 
@@ -74,17 +82,26 @@ const SignupForm = ({ handleSignup, error, isLoading, formRef }) => {
               onInput={(e) => checkPwdRequirements(e.target.value)}
             />
             <i className="absolute right-3 top-1 text-2xl hover:cursor-pointer" onClick={handleToggleEye}>{showEye ? <BsEye /> : <BsEyeSlash />}</i>
-            <ul className="lowercase text-xs text-red-500 py-3 flex flex-col gap-1">
-              {pwdNotMatchingCriteria.includes("length") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-200/75 border border-red-500 rounded">✗ password is not 8-12 characters</li> : <li className="text-emerald-600 bg-green-200/50 p-2 border rounded border-green-500">✓ password is 8 characters or longer</li>}
-              {pwdNotMatchingCriteria.includes("number") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-400/25 border border-red-500 rounded">✗ password needs at least one (1) number</li> : <li className="text-emerald-600 bg-green-200/50 p-2 border rounded border-green-500">✓ password includes at least one (1) number</li>}
-              {pwdNotMatchingCriteria.includes("lowercase") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-400/25 border border-red-500 rounded">✗ password needs at least one (1) lowercase letter</li> : <li className="text-emerald-600 bg-green-200/50 p-2 border rounded border-green-500">✓ password includes at least one (1) lowercase letter</li>}
-              {pwdNotMatchingCriteria.includes("uppercase") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-400/25 border border-red-500 rounded">✗ password needs at least one (1) uppercase letter</li> : <li className="text-emerald-600 bg-green-200/50 p-2 border rounded border-green-500">✓ password includes at least one (1) uppercase letter</li>}
-              {pwdNotMatchingCriteria.includes("special") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-400/25 border border-red-500 rounded">✗ password needs at least one (1) special character</li> : <li className="text-emerald-600 bg-green-200/50 p-2 border rounded border-green-500">✓ password includes at least one (1) special character</li>}
-            </ul>
           </div>
-          <button disabled={isLoading} className='transition-color ease-in-out bg-pink-400 p-2 text-white rounded-md font-bold uppercase transition-colors hover:bg-pink-200 hover:text-pink-800'>sign up</button>
+          <div className="form-layout">
+            <input 
+              type="password" 
+              name="password-validation"
+              placeholder="retype password" 
+              onInput={(e) => checkMatchingPwd(e.target.value)}
+            />
+          </div>
+          {/*start password validation msgs*/}
+          <ul className="lowercase text-xs text-red-600 py-3 flex flex-col gap-1">
+              {pwdNotMatchingCriteria.includes("length") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-100 border border-red-500 rounded">✗ password is not 8-12 characters</li> : <li className="text-emerald-600 bg-green-100 p-2 border rounded border-green-500">✓ password is 8 characters or longer</li>}
+              {pwdNotMatchingCriteria.includes("number") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-100 border border-red-500 rounded">✗ password needs at least one (1) number</li> : <li className="text-emerald-600 bg-green-100 p-2 border rounded border-green-500">✓ password includes at least one (1) number</li>}
+              {pwdNotMatchingCriteria.includes("lowercase") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-100 border border-red-500 rounded">✗ password needs at least one (1) lowercase letter</li> : <li className="text-emerald-600 bg-green-100 p-2 border rounded border-green-500">✓ password includes at least one (1) lowercase letter</li>}
+              {pwdNotMatchingCriteria.includes("uppercase") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-100 border border-red-500 rounded">✗ password needs at least one (1) uppercase letter</li> : <li className="text-emerald-600 bg-green-100 p-2 border rounded border-green-500">✓ password includes at least one (1) uppercase letter</li>}
+              {pwdNotMatchingCriteria.includes("special") || pwdNotMatchingCriteria === "none" ? <li className="p-2 bg-rose-100 border border-red-500 rounded">✗ password needs at least one (1) special character</li> : <li className="text-emerald-600 bg-green-100 p-2 border rounded border-green-500">✓ password includes at least one (1) special character</li>}
+            </ul>
+          <button disabled={isLoading} className='transition-color ease-in-out bg-pink-400 p-2 text-white rounded-md font-bold uppercase transition-colors hover:bg-white hover:text-pink-400'>sign up</button>
         </form>
-        {error && <div className='border border-red-600 rounded-md p-4 mt-3 bg-red-300/25 text-red-600 text-center'>{error}</div>}
+        {error && <div className='border border-red-600 rounded p-4 mt-3 bg-red-300/25 text-red-600 text-center'>{error}</div>}
       </div>
     </div>
   )
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,33 +1,39 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useSignup } from '../hooks/useSignup';
 import SignupForm from "../components/forms/SignupForm";
 
 const Signup = ({ handleSetGreeting }) => {
-      //set reference to form element
-    const formRef = useRef();
-    const { signupUser, error, isLoading } = useSignup();
+  const [isPassingPwd, setIsPassingPwd] = useState(false);
+  const [pwdNotMatchingCriteria, setPwdNotMatchingCriteria] = useState("none");
+  
+  //set reference to form element
+  const formRef = useRef();
+  const { signupUser, error, isLoading } = useSignup();
 
-    const handleSignup = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(formRef.current)//grab the form data using useRef
-        const body = {} //we're going to organize our formdata into body and return that
-
-        formData.forEach((value, key) => {
-          body[key] = value
-        }, {})
-
-        console.log(body)
-
-        await signupUser(body)
-        handleSetGreeting();
-      };
-
-    return (
-        <div className="home-login-signup-w-h grid relative place-content-center">
-            <SignupForm handleSignup={handleSignup} error={error} isLoading={isLoading} formRef={formRef}/>
-        </div>
-    )
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current)//grab the form data using useRef
+    const body = {} //we're going to organize our formdata into body and return that
+    formData.forEach((value, key) => {
+      body[key] = value
+    }, {})
+    console.log(body)
+    await signupUser(body)
+    handleSetGreeting();
+  };
+  return (
+    <div className="home-login-signup-w-h grid relative place-content-center">
+      <SignupForm 
+        handleSignup={handleSignup}
+        setIsPassingPwd={setIsPassingPwd}
+        isPassingPwd={isPassingPwd}
+        setPwdNotMatchingCriteria={setPwdNotMatchingCriteria}
+        pwdNotMatchingCriteria={pwdNotMatchingCriteria} 
+        error={error} 
+        isLoading={isLoading} 
+        formRef={formRef}/>
+    </div>
+  )
 }
 
 export default Signup
